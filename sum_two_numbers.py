628dd8faf8efc03b9506eaae22c0132823f1ccf3
lucky935 <84118606+lucky935@users.noreply.github.com>
2021-05-28 17:55:15 +0530

    Create sum_two_numbers.py

---
+++ sum_two_numbers.py
@@ -0,0 +1,19 @@
+""""
+A function called sum_two that takes input
+two inputs and it returns the sum of thosetwo numbers.
+function name: sum_two
+input: number_1. number_2
+output: number_1 + number_2
+
+
+use --> sum_two(1,3)
+expected output -->3
+
+"""
+def sum_two(number_1,number_2):
+	return number_1 + number_2
+
+
+
+print(sum_two(1,3))
+
