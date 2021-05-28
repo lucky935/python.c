ae404cbb47830b91fa1f6326fe1b18c4af55c38e
lucky935 <84118606+lucky935@users.noreply.github.com>
2021-05-28 07:18:35 +0530

    Create dictationaries.py

---
+++ dictationaries.py
@@ -0,0 +1,14 @@
+#dictionaries
+#DICT[key] -->Value 
+#{key1: value1, key2: vslue2,...}
+#list[0] --> 0th element of the list
+
+
+phone_book = {
+	'lucky': ['123-456', 'lucky@,com'],
+	'papa': ['222-2222-2222', 'papa@.com'],
+	'mom': ['333-333-3333', 'mom@.com']
+}
+
+print(phone_book['papa'][0])
+
